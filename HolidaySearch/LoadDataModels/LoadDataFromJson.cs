a5da83fea7726e1ea6d5cbdc61d168f1276b0ab9
vahidkianfar <vahidkianfar@gmail.com>
2022-07-23 15:09:19 +0100

    Passed test for Parsing Json file into a Flight Object.

--- HolidaySearch/LoadDataModels/LoadDataFromJson.cs
+++ HolidaySearch/LoadDataModels/LoadDataFromJson.cs
@@ -15,14 +15,14 @@ public class LoadDataFromJson
 
     public static List<Flights>? LoadFlights()
     {
-        var flightData = File.ReadAllText(DatasetPath + "HolidaySearch/Datasets/Flights.json");
+        var flightData = File.ReadAllText(DatasetPath + "/HolidaySearch/Datasets/Flights.json");
         var flights = JsonSerializer.Deserialize<List<Flights>>(flightData);
         return flights;
     }
     
     public static List<Hotels>? LoadHotels()
     {
-        var hotelData = File.ReadAllText(DatasetPath + "HolidaySearch/Datasets/Hotels.json");
+        var hotelData = File.ReadAllText(DatasetPath + "/HolidaySearch/Datasets/Hotels.json");
         var hotels = JsonSerializer.Deserialize<List<Hotels>>(hotelData);
         return hotels;
     }
---
+++ HolidayTests/ParsingToObjects/JsonToObjectTests.cs
@@ -0,0 +1,23 @@
+﻿using HolidaySearch.LoadDataModels;
+using FluentAssertions;
+using HolidaySearch.Models;
+namespace HolidayTests.ParsingToObjects;
+
+public class JsonToObjectTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void LoadDataFromJson_Should_Successfully_Parse_JsonData_To_Flight_Object()
+    {
+        var flight = LoadDataFromJson.LoadFlights();
+        Assert.That(flight, Is.Not.Null);
+        Assert.IsInstanceOf<Flights>(flight.First());
+        flight.Count.Should().Be(12);
+        flight.First().FlightId.Should().Be(1);
+        flight.First().OriginCity.Should().Be("MAN");
+    }
+}
