cb4f393de706b354f361a80ddca363be4ae5cb54
vahidkianfar <vahidkianfar@gmail.com>
2022-07-23 19:42:33 +0100

    Green: Added Test for Retrieve Total Prie and Refactored the method.

--- HolidayTests/HolidaySearchTests/HolidaySearchTests.cs
+++ HolidayTests/HolidaySearchTests/HolidaySearchTests.cs
@@ -43,9 +43,34 @@ public class HolidaySearchTests
         departureDate: "2023/07/01",
         duration: 7
         );
-
         holidaySearch.Result().FlightsList.Count.Should().Be(2);
         holidaySearch.Result().HotelsList.Count.Should().Be(1);
+        
+        
     }
-    
+
+    [Test]
+    public void HolidaySearch_Should_Return_Cheapest_Flights_and_Hotels_From_LGW_To_AGP_On_Specific_Date()
+    {
+        var holidaySearch = new HolidaySearch.SearchModels.HolidaySearch(
+            departingFrom: "LGW",
+            travelingTo: "AGP",
+            departureDate: "2023/07/01",
+            duration: 7
+        );
+
+        var expectedFlightId = 11;
+        var expectedHotelId = 9;
+        decimal expectedTotalPrice = 736;
+
+        var resultFlightId = holidaySearch.Result().FlightsList.First().FlightId;
+        var resultHotelId = holidaySearch.Result().HotelsList.First().HotelId;
+        var resultTotalPrice = holidaySearch.Result().ListOfTotalPrices.First();
+
+        resultFlightId.Should().Be(expectedFlightId);
+        resultHotelId.Should().Be(expectedHotelId);
+        resultTotalPrice.Should().Be(expectedTotalPrice);
+
+    }
+
 }
