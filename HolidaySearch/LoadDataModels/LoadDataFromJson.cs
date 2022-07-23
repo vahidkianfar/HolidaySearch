580e67d7c2837c87814da621c5ae212b18451af6
vahidkianfar <vahidkianfar@gmail.com>
2022-07-23 14:54:55 +0100

    Added LoadFromJson class to Parse the json file and converted it into List<Flights> and List<Hotels>.

---
+++ HolidaySearch/LoadDataModels/LoadDataFromJson.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using HolidaySearch.Models;
+
+namespace HolidaySearch.LoadDataModels;
+
+public class LoadDataFromJson
+{
+    private static readonly  DirectoryInfo? DatasetPath =
+        Directory.GetParent(Directory.GetParent(
+            Directory.GetParent(
+                Directory.GetParent(
+                        Directory.GetCurrentDirectory())?
+                    .ToString() ?? string.Empty)?.ToString()
+            ?? string.Empty)?.ToString() ?? string.Empty);
+
+    public static List<Flights>? LoadFlights()
+    {
+        var flightData = File.ReadAllText(DatasetPath + "HolidaySearch/Datasets/Flights.json");
+        var flights = JsonSerializer.Deserialize<List<Flights>>(flightData);
+        return flights;
+    }
+    
+    public static List<Hotels>? LoadHotels()
+    {
+        var hotelData = File.ReadAllText(DatasetPath + "HolidaySearch/Datasets/Hotels.json");
+        var hotels = JsonSerializer.Deserialize<List<Hotels>>(hotelData);
+        return hotels;
+    }
+
+}
