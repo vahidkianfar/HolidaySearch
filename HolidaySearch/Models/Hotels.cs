4310c147e98660122fcc27a551ddcd0528fb5c50
vahidkianfar <vahidkianfar@gmail.com>
2022-07-23 13:10:19 +0100

    Created Hotel Class matched with Json Properties.

---
+++ HolidaySearch/Models/Hotels.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HolidaySearch.Models;
+
+public class Hotels
+{
+    [JsonPropertyName("id")]
+    public int HotelId { get; set; }
+    
+    [JsonPropertyName("name")]
+    public string HotelName { get; set; }
+    
+    [JsonPropertyName("arrival_date")]
+    public string ArrivalDate { get; set; }
+    
+    [JsonPropertyName("price_per_night")]
+    public decimal HotelPricePerNight { get; set; }
+    
+    [JsonPropertyName("local_airport")]
+    public List<string> LocalAirport { get; set; }
+    
+    [JsonPropertyName("nights")]
+    public int DurationNights { get; set; }
+    
+}
