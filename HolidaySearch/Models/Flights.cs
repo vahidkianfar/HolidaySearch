f0b12a68531d97e9c7526dba8eafd7e71f64c488
vahidkianfar <vahidkianfar@gmail.com>
2022-07-23 13:15:52 +0100

    Created Flights Class based on Json Properties.

---
+++ HolidaySearch/Models/Flights.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HolidaySearch.Models;
+
+public class Flights
+{
+    [JsonPropertyName("id")]
+    public int FlightId { get; set; }
+    
+    [JsonPropertyName("airline")]
+    public string AirlineName { get; set; }
+    
+    [JsonPropertyName("from")]
+    public string OriginCity { get; set; }
+    
+    [JsonPropertyName("to")]
+    public string DestinationCity { get; set; }
+    
+    [JsonPropertyName("price")]
+    public decimal FlightPrice { get; set; }
+    
+    [JsonPropertyName("departure_date")]
+    
+    public DateTime DepartureDate { get; set; }
+    
+}
