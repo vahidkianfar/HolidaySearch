1c5307c131c9b3aec8a30c6d5b33d32080cc33a9
vahidkianfar <vahidkianfar@gmail.com>
2022-07-23 15:24:10 +0100

    Created HolidaySearch class.

---
+++ HolidaySearch/SearchModels/HolidaySearch.cs
@@ -0,0 +1,17 @@
+﻿namespace HolidaySearch.SearchModels;
+
+public class HolidaySearch
+{
+    public string DepartingFrom { get; set; }
+    public string TravelingTo { get; set; }
+    public string DepartingDate { get; set; }
+    public int Duration { get; set; }
+    
+    public HolidaySearch(string departingFrom, string travelingTo, string departingDate, int duration)
+    {
+        DepartingFrom = departingFrom;
+        TravelingTo = travelingTo;
+        DepartingDate = departingDate;
+        Duration = duration;
+    }
+}
