287517e8b7419a643b6ecfc3111800b2c363ac18
vahidkianfar <vahidkianfar@gmail.com>
2022-07-23 19:10:41 +0100

    Added Method for Ascending Sort for List<Flights> and List<Hotels>.

--- HolidaySearch/SearchModels/HolidaySearch.cs
+++ HolidaySearch/SearchModels/HolidaySearch.cs
@@ -23,89 +23,63 @@ public class HolidaySearch
         var cheapest = new MatchFlightsAndHotels
         {
             
-            // FlightsList = ReturnFlightPriceByAscendingOrder(),
-            // HotelsList = ReturnHotelPriceByAscendingOrder(),
-            // ListOfTotalPrices = CalculateListOfTotalPrice
+            FlightsList = ReturnFlightPriceByAscendingOrder(),
+            HotelsList = ReturnHotelPriceByAscendingOrder(),
+            ListOfTotalPrices = CalculateListOfTotalPrice()
 
         };
 
         return cheapest;
     }
     
-    public static List<Flights> FindMatchFlights(string originCity, string destinationCity, string departureDate)
+    private List<Flights> ReturnFlightPriceByAscendingOrder()
     {
-        var flights = LoadDataFromJson.LoadFlights();
-        List<Flights> match;
-
-        if (originCity.Contains("London"))
-        {
-            var listOfLondonAirport = GetLondonAirports();
-            match =
-                flights.Where(flight =>
-                    listOfLondonAirport.Contains(flight.OriginCity)
-                    && flight.DestinationCity == destinationCity &&
-                    flight.DepartureDate == DateTime.Parse(departureDate)).ToList();
-        }
-        
-        else if (originCity=="Any Airport")
-        {
-            match =
-                flights.Where(flight =>
-                    flight.DestinationCity == destinationCity &&
-                    flight.DepartureDate == DateTime.Parse(departureDate)).ToList();
-        }
-        
-        else
-        {
-            match =
-                flights.Where(flight =>
-                    flight.OriginCity == originCity && flight.DestinationCity == destinationCity &&
-                    flight.DepartureDate == DateTime.Parse(departureDate)).ToList();
-        }
-
-        return match;
+        var matchFlights = Flights.FindMatchFlights(DepartingFrom, TravelingTo, DepartureDate);
+        matchFlights.Sort((flightA, flightB) => flightA.FlightPrice.CompareTo(flightB.FlightPrice));
+        return matchFlights;
     }
 
-    public static List<Hotels> FindMatchHotels(string destinationCity, int duration , string departureDate)
+    private List<Hotels> ReturnHotelPriceByAscendingOrder()
     {
-        var hotels = LoadDataFromJson.LoadHotels();
-        var match = hotels.Where(hotel => hotel.LocalAirports.First() == destinationCity
-                                          && hotel.DurationNights == duration && 
-                                          hotel.ArrivalDate == DateTime.Parse(departureDate)).ToList() ;
-        return match;
+        var matchHotels = Hotels.FindMatchHotels(TravelingTo, Duration, DepartureDate);
+        matchHotels.Sort((hotelA, hotelB) => hotelA.HotelPricePerNight.CompareTo(hotelB.HotelPricePerNight));
+        return matchHotels;
     }
     
-    private static List<string> GetLondonAirports()
+    private List<decimal> CalculateListOfTotalPrice()
     {
-        var listOfLondonAirport = new List<string> { "LGW", "LTN" };
-        return listOfLondonAirport;
+        var flightPrice = ReturnFlightPriceByAscendingOrder().Select(flight => flight.FlightPrice).ToList();
+        var hotelPrice = ReturnHotelPriceByAscendingOrder().Select(hotel => hotel.HotelPricePerNight*Duration).ToList();
+       
+        return (from flight in flightPrice from hotel in hotelPrice select flight + hotel).Distinct().ToList();
     }
     
-    
-
-    public List<int> OldResult()
-    {
-        var listOfFlights = LoadDataFromJson.LoadFlights();
-        var listOfHotels = LoadDataFromJson.LoadHotels();
-        
-        var matchFlight = listOfFlights.Where(flight =>
-            flight.OriginCity == DepartingFrom &&
-            flight.DestinationCity == TravelingTo &&
-            flight.DepartureDate == DateTime.Parse(DepartureDate)).ToList();
-        
-        matchFlight.Sort((flightA, flightB) => flightA.FlightPrice.CompareTo(flightB.FlightPrice));
-        
-        var matchHotel=listOfHotels.Where(hotel=>
-            hotel.LocalAirports.First()==TravelingTo &&
-            hotel.DurationNights==Duration &&
-            hotel.ArrivalDate == DateTime.Parse(DepartureDate)).ToList();
-        
-        matchHotel.Sort((hotelA, hotelB) => hotelA.HotelPricePerNight.CompareTo(hotelB.HotelPricePerNight));
-        
-        var result = new List<int>();
-        result.Add(matchFlight.First().FlightId);
-        result.Add(matchHotel.First().HotelId);
-
-        return result;
-    }
+    //
+    //
+    //
+    // public List<int> OldResult()
+    // {
+    //     var listOfFlights = LoadDataFromJson.LoadFlights();
+    //     var listOfHotels = LoadDataFromJson.LoadHotels();
+    //     
+    //     var matchFlight = listOfFlights.Where(flight =>
+    //         flight.OriginCity == DepartingFrom &&
+    //         flight.DestinationCity == TravelingTo &&
+    //         flight.DepartureDate == DateTime.Parse(DepartureDate)).ToList();
+    //     
+    //     matchFlight.Sort((flightA, flightB) => flightA.FlightPrice.CompareTo(flightB.FlightPrice));
+    //     
+    //     var matchHotel=listOfHotels.Where(hotel=>
+    //         hotel.LocalAirports.First()==TravelingTo &&
+    //         hotel.DurationNights==Duration &&
+    //         hotel.ArrivalDate == DateTime.Parse(DepartureDate)).ToList();
+    //     
+    //     matchHotel.Sort((hotelA, hotelB) => hotelA.HotelPricePerNight.CompareTo(hotelB.HotelPricePerNight));
+    //     
+    //     var result = new List<int>();
+    //     result.Add(matchFlight.First().FlightId);
+    //     result.Add(matchHotel.First().HotelId);
+    //
+    //     return result;
+    // }
 }
