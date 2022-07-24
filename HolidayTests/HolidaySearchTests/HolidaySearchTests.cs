3a58951ee63450c6cbfd885c9b3d3ba496e6ea0d
vahidkianfar <vahidkianfar@gmail.com>
2022-07-24 01:22:47 +0100

    Added New Unit Tests.

--- HolidayTests/HolidaySearchTests/HolidaySearchTests.cs
+++ HolidayTests/HolidaySearchTests/HolidaySearchTests.cs
@@ -1,5 +1,4 @@
-﻿using HolidaySearch.SearchModels;
-using FluentAssertions;
+﻿using FluentAssertions;
 namespace HolidayTests.HolidaySearchTests;
 
 public class HolidaySearchTests
@@ -47,6 +46,30 @@ public class HolidaySearchTests
         holidaySearch.Result().HotelsList.Count.Should().Be(1);
         
     }
+    [Test]
+    public void HolidaySearch_Should_Return_All_Available_Flights_and_Hotels_From_LGW_To_AGP_Correctly()
+    {
+        var holidaySearch = new HolidaySearch.SearchModels.HolidaySearch(
+            departingFrom: "LGW",
+            travelingTo: "AGP",
+            departureDate: "2023/07/01",
+            duration: 7
+        );
+        
+        var expectedFlightId1 = 11;
+        var expectedFlightId2 = 10;
+        var expectedHotelId = 9;
+        
+        var resultFlightId1 = holidaySearch.Result().FlightsList.First().FlightId;
+        
+        var resultFlightId2 = holidaySearch.Result().FlightsList[1].FlightId;
+        var resultHotelId = holidaySearch.Result().HotelsList.First().HotelId;
+        
+        resultFlightId1.Should().Be(expectedFlightId1);
+        resultFlightId2.Should().Be(expectedFlightId2);
+        resultHotelId.Should().Be(expectedHotelId);
+        
+    }
 
     [Test]
     public void HolidaySearch_Should_Return_Cheapest_Flights_and_Hotels_From_LGW_To_AGP()
@@ -60,15 +83,18 @@ public class HolidaySearchTests
 
         var expectedFlightId = 11;
         var expectedHotelId = 9;
-        decimal expectedTotalPrice = 736;
+        decimal expectedCheapestPrice = 736;
+        decimal expectedSecondCheapestPrice = 806;
 
         var resultFlightId = holidaySearch.Result().FlightsList.First().FlightId;
         var resultHotelId = holidaySearch.Result().HotelsList.First().HotelId;
-        var resultTotalPrice = holidaySearch.Result().ListOfTotalPrices.First();
+        var resultCheapestTotalPrice = holidaySearch.Result().ListOfTotalPrices.First();
+        var resultSecondCheapestTotalPrice = holidaySearch.Result().ListOfTotalPrices[1];
 
         resultFlightId.Should().Be(expectedFlightId);
         resultHotelId.Should().Be(expectedHotelId);
-        resultTotalPrice.Should().Be(expectedTotalPrice);
+        resultCheapestTotalPrice.Should().Be(expectedCheapestPrice);
+        resultSecondCheapestTotalPrice.Should().Be(expectedSecondCheapestPrice);
 
     }
 
@@ -153,7 +179,7 @@ public class HolidaySearchTests
             duration: 14
         );
         
-        holidaySearch.Result().HotelsList?.Count.Should().Be(0);
+        holidaySearch.Result().HotelsList.Count.Should().Be(0);
     }
     
      
@@ -169,7 +195,7 @@ public class HolidaySearchTests
         
          var expectedFlightId = 1;
         
-         var resultFlightId = holidaySearch.Result().FlightsList?.First().FlightId;
+         var resultFlightId = holidaySearch.Result().FlightsList.First().FlightId;
         
          resultFlightId.Should().Be(expectedFlightId);
         
@@ -197,6 +223,106 @@ public class HolidaySearchTests
         resultHotelId.Should().Be(expectedHotelId);
         resultTotalPrice.Should().Be(expectedTotalPrice);
     }
+    
+    [Test]
+    public void HolidaySearch_Should_Return_Cheapest_Flights_and_Hotels_From_LTN_To_PMI()
+    {
+        var holidaySearch = new HolidaySearch.SearchModels.HolidaySearch(
+            departingFrom: "LTN",
+            travelingTo: "PMI",
+            departureDate: "2023/06/15",
+            duration: 14
+        );
+        
+        var expectedFlightId = 4;
+        var expectedHotelId = 3;
+        decimal expectedTotalPrice = 979;
+        
+        var resultFlightId = holidaySearch.Result().FlightsList.First().FlightId;
+        var resultHotelId = holidaySearch.Result().HotelsList.First().HotelId;
+        var resultTotalPrice = holidaySearch.Result().ListOfTotalPrices.First();
+        
+        resultFlightId.Should().Be(expectedFlightId);
+        resultHotelId.Should().Be(expectedHotelId);
+        resultTotalPrice.Should().Be(expectedTotalPrice);
+    }
+    
+    [Test]
+    public void Number_Of_Flights_Should_Be_0_For_Unknown_Departure_Location()
+    {
+        var holidaySearch = new HolidaySearch.SearchModels.HolidaySearch(
+            departingFrom: "ABC",
+            travelingTo: "PMI",
+            departureDate: "2023/06/15",
+            duration: 14
+        );
+        
+       holidaySearch.Result().FlightsList.Count.Should().Be(0);
+    }
+    [Test]
+    public void Number_Of_Flights_Should_Be_0_For_Unknown_Destination_Location()
+    {
+        var holidaySearch = new HolidaySearch.SearchModels.HolidaySearch(
+            departingFrom: "MAN",
+            travelingTo: "ABC",
+            departureDate: "2023/06/15",
+            duration: 14
+        );
+        
+        holidaySearch.Result().FlightsList.Count.Should().Be(0);
+    }
+    
+    [Test]
+    public void Number_Of_Flights_Should_Be_0_For_Out_Of_Scope_Departure_Date()
+    {
+        var holidaySearch = new HolidaySearch.SearchModels.HolidaySearch(
+            departingFrom: "ABC",
+            travelingTo: "PMI",
+            departureDate: "2025/06/15",
+            duration: 14
+        );
+        
+        holidaySearch.Result().FlightsList.Count.Should().Be(0);
+    }
+    
+    [Test]
+    public void Number_Of_Hotels_Should_Be_0_For_Unknown_Destination_Location()
+    {
+        var holidaySearch = new HolidaySearch.SearchModels.HolidaySearch(
+            departingFrom: "MAN",
+            travelingTo: "ABC",
+            departureDate: "2023/06/15",
+            duration: 14
+        );
+        
+        holidaySearch.Result().HotelsList.Count.Should().Be(0);
+    }
+    
+    [Test]
+    public void Number_Of_Hotels_Should_Be_0_For_Out_Of_Scope_Duration()
+    {
+        var holidaySearch = new HolidaySearch.SearchModels.HolidaySearch(
+            departingFrom: "MAN",
+            travelingTo: "PMI",
+            departureDate: "2023/06/15",
+            duration: 35
+        );
+        
+        holidaySearch.Result().HotelsList.Count.Should().Be(0);
+    }
+    
+    [Test]
+    public void Number_Of_Hotels_Should_Be_0_For_Out_Of_Scope_Departure_Date()
+    {
+        var holidaySearch = new HolidaySearch.SearchModels.HolidaySearch(
+            departingFrom: "MAN",
+            travelingTo: "PMI",
+            departureDate: "2025/06/15",
+            duration: 14
+        );
+        holidaySearch.Result().HotelsList.Count.Should().Be(0);
+    }
+
 
 
 }
