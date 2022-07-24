5d971bd4e55059f475dcbea746a724ff58ce0548
vahidkianfar <vahidkianfar@gmail.com>
2022-07-24 01:32:08 +0100

    Added PrettyPrint method and Culture Info to showing total price with GBP symbol.

--- HolidaySearch/SearchModels/HolidaySearch.cs
+++ HolidaySearch/SearchModels/HolidaySearch.cs
@@ -1,5 +1,7 @@
-﻿using HolidaySearch.LoadDataModels;
+﻿
+using System.Globalization;
 using HolidaySearch.Models;
+using Spectre.Console;
 
 namespace HolidaySearch.SearchModels;
 
@@ -30,52 +32,45 @@ public class HolidaySearch
         return cheapest;
     }
     
-    private List<Flights> ReturnFlightPriceByAscendingOrder()
+    public List<Flights> ReturnFlightPriceByAscendingOrder()
     {
-        try
-        {
-            var matchFlights = MatchedFlightsAndHotels.FindMatchFlights(DepartingFrom, TravelingTo, DepartureDate);
-            matchFlights.Sort((flightA, flightB) => flightA.FlightPrice.CompareTo(flightB.FlightPrice));
-            return matchFlights;
-        }
-        catch (ArgumentNullException)
-        {
-            Console.WriteLine("There is no flights available");
-            throw;
-        }
-        
+        var matchFlights = MatchedFlightsAndHotels.FindMatchFlights(DepartingFrom, TravelingTo, DepartureDate);
+        matchFlights.Sort((flightA, flightB) => flightA.FlightPrice.CompareTo(flightB.FlightPrice));
+        return matchFlights;
     }
 
-    private List<Hotels> ReturnHotelPriceByAscendingOrder()
+    public List<Hotels> ReturnHotelPriceByAscendingOrder()
     {
-        try
-        {
-            var matchHotels = MatchedFlightsAndHotels.FindMatchHotels(TravelingTo, Duration, DepartureDate);
-            matchHotels?.Sort((hotelA, hotelB) => hotelA.HotelPricePerNight.CompareTo(hotelB.HotelPricePerNight));
-            return matchHotels;
-        }
-        catch (ArgumentNullException)
-        {
-            Console.WriteLine("There is no hotels available");
-            throw;
-        }
         
-
+        var matchHotels = MatchedFlightsAndHotels.FindMatchHotels(TravelingTo, Duration, DepartureDate);
+        matchHotels.Sort((hotelA, hotelB) => hotelA.HotelPricePerNight.CompareTo(hotelB.HotelPricePerNight));
+        return matchHotels;
     }
     
-    private List<decimal> CalculateListOfTotalPrice()
+    public List<decimal> CalculateListOfTotalPrice()
     {
-        try
-        {
-            var flightPrice = ReturnFlightPriceByAscendingOrder().Select(flight => flight.FlightPrice).ToList();
-            var hotelPrice = ReturnHotelPriceByAscendingOrder().Select(hotel => hotel.HotelPricePerNight*Duration).ToList();
-            return (from flight in flightPrice from hotel in hotelPrice select flight + hotel).Distinct().ToList();
-        }
-        catch (Exception)
-        {
-            Console.WriteLine("Result is not available");
-            throw;
-        }
+        var flightPrice = ReturnFlightPriceByAscendingOrder().Select(flight => flight.FlightPrice).ToList();
+        var hotelPrice = ReturnHotelPriceByAscendingOrder().Select(hotel => hotel.HotelPricePerNight*Duration).ToList();
+        return (from flight in flightPrice from hotel in hotelPrice select flight + hotel).Distinct().ToList();
+    }
+
+    public void PrettyPrint()
+    {
+        Console.Clear();
+        AnsiConsole.Write(
+            new Table()
+                .AddColumn(new TableColumn("Flight ID").Centered())
+                .AddColumn(new TableColumn("Hotel ID").Centered())
+                .AddColumn(new TableColumn("Total Price").Centered())
+                .AddRow($"[skyblue1]{Result().FlightsList.First().FlightId}[/]",
+                    $"[skyblue1]{Result().HotelsList.First().HotelId}[/]",
+                    $"[green]{Result().ListOfTotalPrices.First().ToString("C", new CultureInfo("en-GB"))}[/]")
+                .BorderColor(Color.Yellow2));
         
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+        Console.ResetColor();
+        Console.Clear();
     }
 }
