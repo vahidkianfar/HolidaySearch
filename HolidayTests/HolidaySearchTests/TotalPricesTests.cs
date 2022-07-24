5b7c2db3c7c2b1eb0ac1442a9cf2d4e5257842d6
vahidkianfar <vahidkianfar@gmail.com>
2022-07-24 19:51:09 +0100

    Moved TotalPrices tests to separate file.

---
+++ HolidayTests/HolidaySearchTests/TotalPricesTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+
+namespace HolidayTests.HolidaySearchTests;
+
+public class TotalPricesTests
+{
+    [Test]
+    public void CalculateTotalPrice_Should_Return_Correct_Total_Price_Of_Flights_Hotels()
+    {
+        //---Arrange
+        var holidaySearch = new HolidaySearch.SearchModels.HolidaySearch(
+            departingFrom: "",
+            travelingTo: "AGP",
+            departureDate: "2023/07/01",
+            duration: 7
+        );
+        
+        decimal expectedTotalPrice = 736;
+        
+        //---Act
+        var result= holidaySearch.CalculateListOfTotalPrice();
+        
+        //---Assert
+        result.First().Should().Be(expectedTotalPrice);
+    }
+    
+    [Test]
+    public void CalculateTotalPrice_Should_Return_Total_Price_Of_Flights_Hotels()
+    {
+        var holidaySearch = new HolidaySearch.SearchModels.HolidaySearch(
+            departingFrom: "LTN",
+            travelingTo: "PMI",
+            departureDate: "2023/06/15",
+            duration: 14
+        );
+        decimal expectedTotalPrice = 979;
+        
+        var result= holidaySearch.CalculateListOfTotalPrice();
+        
+        
+        result.First().Should().Be(expectedTotalPrice);
+    }
+}
