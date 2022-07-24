7b99d6aa12b877c3f6d6500678ba50acb1698d2b
vahidkianfar <vahidkianfar@gmail.com>
2022-07-24 01:31:53 +0100

    Added ToUpper for input variables.

--- HolidaySearch/Program.cs
+++ HolidaySearch/Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using HolidaySearch.UI;
 
-Console.WriteLine("Hello, World!");
+MainMenu.Start();
--- HolidaySearch/UI/SearchMenu.cs
+++ HolidaySearch/UI/SearchMenu.cs
@@ -6,18 +6,14 @@ public class SearchMenu
 {
     public static void Start()
     {
-        var departingFrom = AnsiConsole.Ask<string>("Departing from (e.g. [orange1]MAN[/]) : ");
-        var travelingTo = AnsiConsole.Ask<string>("Traveling to (e.g. [orange1]PMI[/]) : ");
-        var departureDate = AnsiConsole.Ask<string>("Departure date (e.g. [orange1]2023-06-15[/]) : ");
-        var duration = AnsiConsole.Ask<int>("Duration (e.g. [orange1]10[/]) : ");
+        var departingFrom = AnsiConsole.Ask<string>("Departing from (e.g. [orange1]MAN[/]) : ").ToUpper();
+        var travelingTo = AnsiConsole.Ask<string>("Traveling to (e.g. [orange1]AGP[/]) : ").ToUpper();
+        var departureDate = AnsiConsole.Ask<string>("Departure date (e.g. [orange1]2023-07-01[/]) : ");
+        var duration = AnsiConsole.Ask<int>("Duration (e.g. [orange1]7[/]) : ");
         
         var holidaySearch = new SearchModels.HolidaySearch(departingFrom, travelingTo, departureDate, duration);
         holidaySearch.PrettyPrint();
         
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("Press any key to continue...");
-        Console.ReadKey();
-        Console.ResetColor();
-        Console.Clear();
+        
     }
 }
