1e0e96b6119dfd7db2e6da84f5cd5a7d663932e4
vahidkianfar <vahidkianfar@gmail.com>
2022-07-24 21:18:26 +0100

    Added UML Diagram

--- HolidaySearch/SearchModels/MatchedFlightsAndHotels.cs
+++ HolidaySearch/SearchModels/MatchedFlightsAndHotels.cs
@@ -9,32 +9,32 @@ public class MatchedFlightsAndHotels
     public List<Hotels> HotelsList { get; set; }
     public List<decimal> ListOfTotalPrices { get; set; }
 
-    public static List<Hotels> FindMatchHotels(string destinationCity, int duration , string departureDate)
+    public static List<Hotels> FindMatchHotels(string travelingTo, int duration , string departureDate)
     {
         var hotels = LoadDataFromJson.LoadHotels();
-        var match = hotels!.Where(hotel => hotel.LocalAirports.First() == destinationCity
+        var match = hotels!.Where(hotel => hotel.LocalAirports.First() == travelingTo
                                            && hotel.DurationNights == duration && 
                                            hotel.ArrivalDate == DateTime.Parse(departureDate)).ToList();
         
         return match;
     }
 
-    public static List<Flights> FindMatchFlights(string originCity, string destinationCity, string departureDate)
+    public static List<Flights> FindMatchFlights(string departingFrom, string travelingTo, string departureDate)
     {
         var flights = LoadDataFromJson.LoadFlights();
         List<Flights> match;
 
-        if (originCity.Contains("London")) 
-            match = ReturnAnyAvailableLondonAirport(flights, destinationCity, departureDate);
+        if (departingFrom.Contains("London")) 
+            match = ReturnAnyAvailableLondonAirport(flights, travelingTo, departureDate);
 
-        else if (originCity=="Any Airport" || string.IsNullOrEmpty(originCity))
-            match = ReturnAnyAvailableAirport(flights, destinationCity, departureDate);
+        else if (departingFrom=="Any Airport" || string.IsNullOrEmpty(departingFrom))
+            match = ReturnAnyAvailableAirport(flights, travelingTo, departureDate);
 
         else
         {
             match =
                 flights!.Where(flight =>
-                    flight.DepartingFrom == originCity && flight.TravelingTo == destinationCity &&
+                    flight.DepartingFrom == departingFrom && flight.TravelingTo == travelingTo &&
                     flight.DepartureDate == DateTime.Parse(departureDate)).ToList();
         }
 
@@ -43,21 +43,21 @@ public class MatchedFlightsAndHotels
 
 
 
-    private static List<Flights> ReturnAnyAvailableLondonAirport(List<Flights> flights , string destinationCity, string departureDate)
+    private static List<Flights> ReturnAnyAvailableLondonAirport(List<Flights> flights , string travelingTo, string departureDate)
     {
         var listOfLondonAirport = GetLondonAirports();
         var match = flights.Where(flight =>
                 listOfLondonAirport.Contains(flight.DepartingFrom!)
-                && flight.TravelingTo == destinationCity &&
+                && flight.TravelingTo == travelingTo &&
                 flight.DepartureDate == DateTime.Parse(departureDate)).ToList();
         
         return match;
     }
-    private static List<Flights> ReturnAnyAvailableAirport(List<Flights> flights , string destinationCity, string departureDate)
+    private static List<Flights> ReturnAnyAvailableAirport(List<Flights> flights , string travelingTo, string departureDate)
     {
         var match =
             flights.Where(flight =>
-                flight.TravelingTo == destinationCity &&
+                flight.TravelingTo == travelingTo &&
                 flight.DepartureDate == DateTime.Parse(departureDate)).ToList();
         
         return match;
