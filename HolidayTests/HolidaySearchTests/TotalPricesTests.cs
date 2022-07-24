76129b9930ba9705377495a8348117135a1c4aa8
vahidkianfar <vahidkianfar@gmail.com>
2022-07-24 21:49:26 +0100

    Added More uni tests for "Calculation Total Price" edge cases.

--- HolidayTests/HolidaySearchTests/TotalPricesTests.cs
+++ HolidayTests/HolidaySearchTests/TotalPricesTests.cs
@@ -5,7 +5,7 @@ namespace HolidayTests.HolidaySearchTests;
 public class TotalPricesTests
 {
     [Test]
-    public void CalculateTotalPrice_Should_Return_Correct_Total_Price_Of_Flights_Hotels()
+    public void CalculateTotalPrice_Should_Return_Correct_Total_Price_Of_Any_Airport_Flights_To_AGP_Hotels()
     {
         //---Arrange
         var holidaySearch = new HolidaySearch.SearchModels.HolidaySearch(
@@ -34,6 +34,23 @@ public class TotalPricesTests
             duration: 14
         );
         decimal expectedTotalPrice = 979;
+        
+        var result= holidaySearch.CalculateListOfTotalPrice();
+        
+        
+        result.First().Should().Be(expectedTotalPrice);
+    }
+    
+    [Test]
+    public void CalculateTotalPrice_Should_Return_Total_Price_Of_Any_London_Airport_Flights_To_PMI_Hotels()
+    {
+        var holidaySearch = new HolidaySearch.SearchModels.HolidaySearch(
+            departingFrom: "Any London Airport",
+            travelingTo: "PMI",
+            departureDate: "2023/06/15",
+            duration: 14
+        );
+        decimal expectedTotalPrice = 901;
         
         var result= holidaySearch.CalculateListOfTotalPrice();
         
